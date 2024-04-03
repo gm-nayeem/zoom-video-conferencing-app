595c0d433e3a35bd1d633890f6b2c3b8cd3a544f
golam mostafa <gmnayeem25@gmail.com>
2024-04-04 01:41:10 +0600

    setup zoom using getstream website

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SiginInPage() {
   return (
-    <main className="flex h-screen w-full items-center justify-center">
+    <main className="flex min-h-screen w-full items-center justify-center">
       <SignIn />
     </main>
   );
