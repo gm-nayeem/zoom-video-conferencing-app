835a46eefba1e8ff2ad1da7a370a2e489651792d
golam mostafa <gmnayeem25@gmail.com>
2024-06-28 15:48:20 +0600

    modify code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from "react";
 import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
-import "./globals.css";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 
+import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
